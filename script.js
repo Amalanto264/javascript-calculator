8c2e14de6bf35d34e72531862cb66ce1fb6a6585
Amalanto264 <60357473+Amalanto264@users.noreply.github.com>
2021-07-14 14:48:11 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+function buttonclick(val)
+{
+    console.log(val)
+    document.getElementById("screen").value=document.getElementById("screen").value+val
+
+}
+
+function cleardisplay()
+{
+    document.getElementById("screen").value=""
+}
+
+function evaluationclick()
+{
+    var text=document.getElementById("screen").value
+    var result=eval(text)
+    document.getElementById("screen").value= result
+}
